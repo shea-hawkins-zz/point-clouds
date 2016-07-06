94a531f2a507b5e883dc1606cb36593c1c0346c5
Shea Hawkins <Shea Hawkins>
2016-07-05 20:39:02 -0700

    Connect application to redux store

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,12 +1,39 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import Header from './Header';
 import PointCloudView from './PointCloudView';
 
-export default (props) => {
-  return (
-    <div>
-      <Header />
-      <PointCloudView />
-    </div>
-  );
+
+class App extends React.Component {
+  componentWillMount() {
+    this.props.getData();
+  }
+  render() {
+    console.log(this.props);
+    return (
+      <div>
+        <Header />
+        {this.props.cloudLoading ? <div>Spinner</div>: <PointCloudView/>}
+      </div>
+    );
+  }
 };
+
+var mapStateToProps = function(state) {
+  return {
+    pointCloud: state.pointCloud,
+    cloudLoading: state.cloudLoading
+  };
+};
+
+var mapDispatchToProps = function(dispatch) {
+  return {
+    getData: function() {
+        dispatch({type: 'loadingCloud'});
+        dispatch({type: 'receiveCloud'});
+        dispatch({type: 'loadedCloud'});
+    }
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
