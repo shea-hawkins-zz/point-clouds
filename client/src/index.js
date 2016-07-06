bb2f260a8ac7ead9d3cd900dfa653fdef1cebc93
Shea Hawkins <Shea Hawkins>
2016-07-05 20:38:29 -0700

    Add redux store and provider

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+var defaultState = {
+  cloudLoading: true,
+  pointCloud: []
+};
+
+var reducerMap = {
+  loadingCloud: function(prevState, data) {
+    return Object.assign({}, prevState, { cloudLoading: true });
+  },
+  loadedCloud: function(prevState, data) {
+    return Object.assign({}, prevState, { cloudLoading: false });
+  },
+  receiveCloud: function(prevState, data) {
+    return Object.assign({}, prevState, { pointCloud: ['I loaded!'] });
+  }
+};
+
+var reducer = function(prevState = defaultState, action) {
+  if (reducerMap[action.type]) {
+    return reducerMap[action.type](prevState, action.data);
+  } else {
+    return prevState;
+  }
+};
+
+var store = createStore(reducer);
+
+ReactDOM.render(
+<Provider store={store}>
+  <App />
+</Provider>
+, document.getElementById('app'));
